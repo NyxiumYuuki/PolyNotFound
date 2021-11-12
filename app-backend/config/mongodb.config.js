7f45c525b000f07fa3984012a2b215f2794297eb
Yûki Vachot <yuki@vachot.fr>
2021-11-12 15:23:47 +0100

    Update

--- app-backend/config/mongodb.config.js
+++ app-backend/config/mongodb.config.js
@@ -1,4 +1,4 @@
 module.exports = {
-  prodUrl: "mongodb://mongodb:27017/polynotfound",
+  prodUrl: "mongodb+srv://nyxiumyuuki:"+process.env.DB_PASSWORD+"@polynotfound.gd5tw.mongodb.net/polynotfound",
   devUrl: "mongodb://127.0.0.1:27017/polynotfound"
 };
