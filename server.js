00ab283d562030e79ce1ac8b79f5f80ab295a089
Yûki Vachot <yuki@vachot.fr>
2021-11-12 15:40:11 +0100

    Update

--- server.js
+++ server.js
@@ -28,8 +28,8 @@ db.mongoose
     }
   });
 
-app.use(express.static(__dirname + '/dist/frontend'));
-app.get('/*', function(req,res) {
+//app.use(express.static(__dirname + '/dist/frontend'));
+app.get('/', function(req,res) {
   res.sendFile(path.join(__dirname+ '/dist/frontend/index.html'));
 });
 
