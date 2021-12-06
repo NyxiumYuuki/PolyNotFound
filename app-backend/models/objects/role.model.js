9ab394244bb22c91f3b0f5138560caa8138a8ad9
Yûki Vachot <yuki@vachot.fr>
2021-12-06 05:38:31 +0100

    Update: add isAccepted field

--- app-backend/models/objects/role.model.js
+++ app-backend/models/objects/role.model.js
@@ -1,18 +1,22 @@
 module.exports = {
   User: {
     name: "user",
-    permission: 0
+    permission: 0,
+    isAccepted: true
   },
   Advertiser: {
       name: "advertiser",
-      permission: 5
+      permission: 5,
+      isAccepted: false
   },
   Admin: {
     name: "admin",
-    permission: 10
+    permission: 10,
+    isAccepted: false
   },
   SuperAdmin: {
     name: "superAdmin",
-    permission: 1000
+    permission: 1000,
+    isAccepted: true
   }
 };
