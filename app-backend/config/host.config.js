e5870a639ae8e16cf4236a38a672d6c55777d3eb
Yûki Vachot <yuki@vachot.fr>
2021-12-10 16:05:57 +0100

    Update: Add baseChannelUrl

--- app-backend/config/host.config.js
+++ app-backend/config/host.config.js
@@ -3,12 +3,14 @@ module.exports = {
     name: "Youtube",
     shortname: "yt",
     baseAPIUrl: 'https://youtube.googleapis.com/youtube/v3',
+    baseChannelUrl: 'https://www.youtube.com/channel/',
     YOUTUBE_API_KEY: process.env.YOUTUBE_API_KEY
   },
   dailymotion: {
     name: "Dailymotion",
     shortname: "dm",
-    baseAPIUrl: 'https://api.dailymotion.com/',
+    baseAPIUrl: 'https://api.dailymotion.com',
+    baseChannelUrl: 'https://www.dailymotion.com/',
     DAILYMOTION_API_KEY: process.env.DAILYMOTION_API_KEY
   }
 };
