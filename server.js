5024b297ec55456185ab87e8577894d63ee655b2
Yûki VACHOT <git@vachot.fr>
2021-12-22 13:22:38 +0100

    Update: Environment for Heroku Production

--- server.js
+++ server.js
@@ -3,9 +3,9 @@ const express = require('express');
 const app = express();
 const port = process.env.PORT || 3000;
 
-app.use(express.static(__dirname + '/dist/frontend'));
+app.use(express.static(__dirname + '/dist/frontend-admin'));
 app.get('/*', function(req,res) {
-  res.sendFile(path.join(__dirname+ '/dist/frontend/index.html'));
+  res.sendFile(path.join(__dirname+ '/dist/frontend-admin/index.html'));
 });
 
 app.listen(port, '0.0.0.0',() => {
