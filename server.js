f00762b5fe40e9cfff84999232dcda4715e73c03
Yûki Vachot <yuki@vachot.fr>
2021-12-22 21:11:16 +0100

    Update: Cors

--- server.js
+++ server.js
@@ -3,6 +3,14 @@ const express = require('express');
 const app = express();
 const port = process.env.PORT || 3000;
 
+const cors = require('cors');
+app.use(cors({
+  origin: [
+    'https://api-polynotfound.herokuapp.com/', 'http://127.0.0.1:3000'
+   ], 
+  credentials: true
+}));
+
 app.use(express.static(__dirname + '/dist/frontend-admin'));
 app.get('/*', function(req,res) {
   res.sendFile(path.join(__dirname+ '/dist/frontend-admin/index.html'));
