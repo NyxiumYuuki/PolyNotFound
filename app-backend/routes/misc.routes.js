13daf10ca85a8e89b6c1ac59800a1d322af671e7
Yûki VACHOT <git@vachot.fr>
2021-12-22 13:39:06 +0100

    Update: Environment for Heroku Production

--- app-backend/routes/misc.routes.js
+++
@@ -1,9 +0,0 @@
-const misc = require("../controllers/misc.controller");
-module.exports = app => {
-  let router = require("express").Router();
-
-  // Get all interests available
-  router.get("/misc/getInterests", misc.getInterests);
-
-  app.use('/api', router);
-};
