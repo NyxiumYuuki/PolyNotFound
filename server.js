a12926f277190973cb00b82777285171eb89c3cd
Yûki VACHOT <git@vachot.fr>
2021-12-23 13:10:57 +0100

    Update: Remove *all get request

--- server.js
+++ server.js
@@ -1,4 +1,3 @@
-const path = require('path');
 const express = require('express');
 const app = express();
 const port = process.env.PORT || 3000;
@@ -76,10 +75,6 @@ User.exists({role: roles.SuperAdmin}, function (err, docs){
       console.log("superAdmin already exist !");
     }
   }
-});
-
-app.get('/*all', function(req,res) {
-  res.sendFile(path.join(__dirname+ '/dist/index.html'));
 });
 
 app.listen(port, '0.0.0.0',() => {
