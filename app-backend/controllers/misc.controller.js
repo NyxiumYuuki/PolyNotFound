d1b3829450e8b97c1df2ae1718962d6191775573
Yûki VACHOT <git@vachot.fr>
2021-12-25 00:59:53 +0100

    Update: README

--- app-backend/controllers/misc.controller.js
+++
@@ -1,7 +0,0 @@
-const {sendMessage} = require ("../config/response.config");
-const interests = require("../models/objects/video.categories.model");
-
-// Get all interests available
-exports.getInterests = (req, res) => {
-  return sendMessage(res, 51, interests, null)
-};
