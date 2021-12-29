f69ed4b3d488b8f18a8ccafe89f96da789166237
Yûki VACHOT <git@vachot.fr>
2021-12-29 19:01:06 +0100

    Update: Npm Cors Doc

--- config/cors.config.js
+++ config/cors.config.js
@@ -1,28 +1,19 @@
 const cors = require('cors');
 module.exports.cors = cors;
 
-const allowList = [
+const whitelist = [
     'http://127.0.0.1:4200',
     'http://127.0.0.1:4201',
     'https://admin-polynotfound.herokuapp.com',
     'https://polynotfound.herokuapp.com'
 ];
 
-const corsOptionsDelegate = function(req, callback) {
-    console.log(req.header('Origin'), allowList.indexOf(req.header('Origin')));
-    let corsOptions;
-    if (allowList.indexOf(req.header('Origin')) !== -1) {
-        corsOptions = {
-            origin: true,
-            credentials: true
-        }
-    } else {
-        corsOptions = {
-            origin: false,
-            credentials: true
+module.exports.corsOptions = {
+    origin: function(origin, callback) {
+        if (whitelist.indexOf(origin) !== -1) {
+            callback(null, true);
+        } else {
+            callback(new Error('Not allowed by CORS'));
         }
     }
-    console.log(corsOptions);
-    callback(null, corsOptions)
 }
-module.exports.options = corsOptionsDelegate;
