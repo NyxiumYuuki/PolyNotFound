68e212fce97d2a302e438aae976656fa3a107c51
Yûki VACHOT <git@vachot.fr>
2021-12-29 17:52:59 +0100

    Update: Rollback Origin

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const port = process.env.PORT || 3000;
 
 const cors = require('cors');
 app.use(cors({
-  origin: '*',
+  origin: 'https://admin-polynotfound.herokuapp.com',
   credentials: true
 }));
 
